d3ffacaea28d28c9433378fc2e65c182d2c2b712
andycv587 <andy.sh.cao@gmail.com>
2024-05-28 09:23:33 -0700

    For Grading Purpose

--- grade.sh
+++ grade.sh
@@ -1,4 +1,4 @@
-CPATH='.:lib/hamcrest-core-1.3.jar:lib/junit-4.13.2.jar'
+CPATH=".;lib/junit-4.13.2.jar;lib/hamcrest-core-1.3.jar"
 
 rm -rf student-submission
 rm -rf grading-area
@@ -8,9 +8,20 @@ mkdir grading-area
 git clone $1 student-submission
 echo 'Finished cloning'
 
+# Corrected if statement with proper spacing
+if [ -f student-submission/ListExamples.java ]; then
+    echo "File Existed"
+else
+    echo "ListExamples.java Doesn't exist"
+    echo "grade: 0"
+    exit 1
+fi
 
-# Draw a picture/take notes on the directory structure that's set up after
-# getting to this point
+cp student-submission/ListExamples.java grading-area
+cp TestListExamples.java grading-area
+cp -r lib grading-area
 
-# Then, add here code to compile and run, and do any post-processing of the
-# tests
+cd grading-area
+javac -cp $CPATH ListExamples.java TestListExamples.java
+
+echo "$?"
